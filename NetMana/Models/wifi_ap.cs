5a2f4552b5a6848d13f3618918fcfd2e5ed9e882
unicorn <borivojmarek@gmail.com>
2026-05-22 09:54:17 +0200

    added unversioned files

---
+++ NetMana/Models/wifi_ap.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using NetworkManager.DBus;
+using Tmds.DBus;
+
+namespace NetMana.Models;
+
+public partial class wifi_ap : ObservableObject
+{
+    [ObservableProperty]
+    private string _ssid = string.Empty;
+
+    [ObservableProperty]
+    private string _password = string.Empty;
+
+    [ObservableProperty]
+    private bool _isConnectionRefused;
+    
+    [ObservableProperty]
+    private byte _strength;
+
+    public string MacAddress { get; set; } = string.Empty;
+    public ObjectPath Path { get; set; }
+
+    public int StrengthInt => (int)Strength;
+    
+    public static async Task<List<wifi_ap>> GetSsidsInRangeAsync()
+    {
+        var connection = new Connection(Address.System);
+        await connection.ConnectAsync();
+
+        var networkManager = connection.CreateProxy<INetworkManager>(
+            "org.freedesktop.NetworkManager",
+            "/org/freedesktop/NetworkManager");
+
+        var devicePaths = await networkManager.GetDevicesAsync();
+
+        ObjectPath wifiDevicePath = default;
+
+        foreach (var devicePath in devicePaths)
+        {
+            var device = connection.CreateProxy<IDevice>(
+                "org.freedesktop.NetworkManager",
+                devicePath);
+
+            var deviceType = await device.GetDeviceTypeAsync();
+
+            if (deviceType == 2) // NM_DEVICE_TYPE_WIFI
+            {
+                wifiDevicePath = devicePath;
+                break;
+            }
+        }
+
+        if (wifiDevicePath == default)
+            return new List<wifi_ap>();
+
+        var wireless = connection.CreateProxy<IWireless>(
+            "org.freedesktop.NetworkManager",
+            wifiDevicePath);
+
+        await wireless.RequestScanAsync(new Dictionary<string, object>());
+
+        await Task.Delay(3000);
+
+        var accessPointPaths = await wireless.GetAccessPointsAsync();
+
+        var result = new List<wifi_ap>();
+
+        foreach (var accessPointPath in accessPointPaths)
+        {
+            var accessPoint = connection.CreateProxy<IAccessPoint>(
+                "org.freedesktop.NetworkManager",
+                accessPointPath);
+
+            var props = await accessPoint.GetAllAsync();
+
+            var ssid = Encoding.UTF8.GetString(props.Ssid);
+
+            if (string.IsNullOrWhiteSpace(ssid))
+                continue;
+                
+            var res = new wifi_ap();
+            res.Ssid = ssid;
+            res.Strength = props.Strength;
+            res.MacAddress = props.HwAddress;
+            res.Path = accessPointPath;
+            
+            result.Add(res);
+        }
+
+        return result;
+    }
+}
